5118c2f5adde133bb0fe80d696b9dfac8f11536a
Admin <Admin@DESKTOP-NNGOPDC>
2022-12-19 19:31:53 +0530

    Ecomerse_backup

---
+++ Backup/src/com/ecomerse/Maintest.java
@@ -0,0 +1,11 @@
+package com.ecomerse;
+
+public class Maintest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ecomerse ecomerse =new Ecomerse();
+		ecomerse.Ecomerse();
+	}
+
+}
